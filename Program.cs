e20bbba11f9f9d9aed6caa421d32e19edb1e9468
Martin Janiczek <martin@janiczek.cz>
2024-07-20 11:34:36 +0200

    Original version of the example program to work with SmartCGMS
    Credit: @MartinUbl

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GameDummyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SCGMS_Game game = new SCGMS_Game(1, 1, 60000, "testlog.txt");
+
+            for (int i = 0; i < 10; i++)
+            {
+                game.Step();
+                Console.WriteLine(game.BloodGlucose.ToString());
+            }
+
+            game.ScheduleInsulinBolus(8.0, 0.5);
+
+            for (int i = 0; i < 50; i++)
+            {
+                game.Step();
+                Console.WriteLine(game.BloodGlucose.ToString());
+            }
+
+            game.ScheduleCarbohydratesIntake(35.0, 0.25);
+
+            for (int i = 0; i < 50; i++)
+            {
+                game.Step();
+                Console.WriteLine(game.BloodGlucose.ToString());
+            }
+
+            game.Terminate();
+        }
+    }
+}
